a6862206dbd148e79e92e3cf7c9dc349c0da3744
Larry Grutman <larry.grutman@gmail.com>
2017-01-11 01:59:07 +0100

    new mobile navbar + logo background index + features bootstrap fixed

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -11,11 +11,17 @@ $(document).ready(function(){
     time_constant: 1000
   });
 
-  setInterval(function(){
-    $('.carousel').carousel('next');
-  }, carousel_interval);
+  // setInterval(function(){
+  //   $('.carousel').carousel('next');
+  // }, carousel_interval);
 
-  $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 240, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 
   $( '.swipebox' ).swipebox();
 });
